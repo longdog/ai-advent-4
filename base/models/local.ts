6c623aa558b2c2a33abbd5ff6c6dfe3e3c3c0c2b
longdog <linda@longdog.us>
2025-12-11 14:31:19 +0400

    update model

--- base/models/local.ts
+++ base/models/local.ts
@@ -1,22 +1,55 @@
+import { MemorySaver } from "@langchain/langgraph"
 import { ChatOllama } from "@langchain/ollama"
 import { createAgent, ReactAgent } from "langchain"
-import { checkpointer } from "../memory"
-const systemPrompt = `You are a simple and friendly conversational assistant. 
-You specialize in:
-- everyday conversation,
-- simple facts and definitions,
-- short creative writing,
-- short lists and instructions,
-- simple explanations.
-If the question is too complicated or requires deep knowledge, say briefly that you cannot explain fully, and then give a simple version.
+const systemPrompt = `You are a sports data parser. 
+You will receive a string describing a Muay Thai division.
+Your task is to extract the following information:
+- weight group
+- age group
+- sex
+and return it in format:
+weight group: <extracted weight group>;
+age group: <extracted age group>;
+sex: <extracted sex>;
+Do not return any extra text. Only output these three fields with extracted data.
+`
 
-Your goal: provide small, helpful, easy answers.
+const models = [
+  {
+    temperature: 0.9,
+    model: "gemma3:270m-it-qat",
+    metadata: {
+      name: "gemma3:270m-it-qat",
+      quantization: "Q4_0",
+      size: "241MB",
+    },
+  },
+  {
+    model: "gemma3:270m-it-q8_0",
+    temperature: 0.9,
+    metadata: {
+      name: "gemma3:270m-it-q8_0",
+      quantization: "Q8_0",
+      size: "292MB",
+    },
+  },
+]
+
+const question = `
+J 10-11 Male Youth -32kg
+J 10-11 Female Youth -38kg
+U 23 Female +67kg
+S Female Elite 54 kg
+S Male Elite 96 kg
+ELIT Male ELIT -63.5kg
+ELIT Female ELIT +75kg
 `
 
 export function createOlamaClient() {
+  const checkpointer = new MemorySaver()
   const model = new ChatOllama({
-    model: process.env.OLLAMA_MODEL!,
     baseUrl: process.env.OLLAMA_BASE_URL!,
+    ...models[1],
   })
   const agent = createAgent({
     model,
@@ -36,7 +69,7 @@ export async function olamaChat(
       messages,
     },
     {
-      configurable: { thread_id: chatId },
+      configurable: { thread_id: new Date().toISOString() },
     },
   )
 
